8415ef649c8cc38f9788d2da57c325961224faa3
Chandra Vamshi Dasyam <chandravamshidasyam@Chandras-MacBook-Air.local>
2023-07-27 19:30:37 +0200

    added basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom'
 
 function App() {
 
-  return <h1>Hi</h1>
-  
+  return <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<h1>Home</h1>}></Route>
+      <Route path='new' element={<h1>new</h1>}></Route>
+      <Route path='/:id' >
+        <Route index element={<h1>Id</h1>}></Route>
+        <Route path='edit' element={<h1>Edit</h1>}></Route>
+      </Route>
+      <Route path='*' element={<Navigate to={'/'} />}></Route>
+
+    </Routes>
+  </BrowserRouter>
+
+
 }
 
 export default App
